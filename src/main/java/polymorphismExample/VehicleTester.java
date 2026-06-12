41a4fedc373a323fb65e5be537631c189c292535
cahhi <chrissyrodgers20@gmail.com>
2026-06-12 18:14:12 -0500

    Added comments into the VehicleTester file

--- src/main/java/polymorphismExample/VehicleTester.java
+++ src/main/java/polymorphismExample/VehicleTester.java
@@ -5,11 +5,12 @@ public class VehicleTester {
     public static void main (String[] args){
         Vehicle[] vehicles = new Vehicle[3];
 
+        //placing values into the Vehicles array
         vehicles[0] = new Car();
         vehicles[1] = new Plane();
         vehicles[2] = new Boat();
 
-        //Iterate over the array
+        //Iterate over the array to get the speed of each object
         for(int i = 0; i < vehicles.length; i++){
             vehicles[i].speed();
         }
