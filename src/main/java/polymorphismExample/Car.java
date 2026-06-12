96667bda853721503596c85d1f08e43ee42434c2
cahhi <chrissyrodgers20@gmail.com>
2026-06-12 16:43:47 -0500

    Created and populated the Car class

--- src/main/java/polymorphismExample/Car.java
+++ src/main/java/polymorphismExample/Car.java
@@ -1,4 +1,8 @@
 package polymorphismExample;
 
-public class Car {
+public class Car extends Vehicle {
+
+    public Car() {
+        super("Car start", "Car stop", "Car speed", "Car direction");
+    }
 }
