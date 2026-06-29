2f52c5c2d63444b7a21f1a96b652befa11b85f09
cahhi <chrissyrodgers20@gmail.com>
2026-06-29 12:03:02 -0500

    Added variables to GenericsExercise class

--- src/main/java/genericsAndCollections/GenericsExercise.java
+++ src/main/java/genericsAndCollections/GenericsExercise.java
@@ -1,4 +1,22 @@
 package genericsAndCollections;
 
+import java.util.ArrayList;
+
 public class GenericsExercise {
+
+    public static void main (String[] args){
+
+        //Adding an ArrayList
+        ArrayList<Object> variables = new ArrayList<Object>();
+
+        //Defining variables here
+        Double doubleNumber = 1.5;
+        String name = "Sally";
+        Integer intNum = 1;
+        Character letter = 'a';
+
+        //Adding variable into the ArrayList
+
+
+    }
 }
