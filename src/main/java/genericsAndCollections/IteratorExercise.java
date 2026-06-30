71fb0859a903ac8e0da9d9f4f7d2fe90ffa85417
cahhi <chrissyrodgers20@gmail.com>
2026-06-30 09:12:24 -0500

    Created list, added values, iterated over list, and called while loop to display names in IteratorExercise

--- src/main/java/genericsAndCollections/IteratorExercise.java
+++ src/main/java/genericsAndCollections/IteratorExercise.java
@@ -1,4 +1,27 @@
 package genericsAndCollections;
 
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+
 public class IteratorExercise {
+
+    public static void main (String[] args){
+
+        //Creating a list
+        List<String> names = new LinkedList<String>();
+
+        //adding in variables
+        names.add("Mike");
+        names.add("Bob");
+        names.add("Alice");
+
+        //using the iterator interface to traverse over all the values
+        Iterator<String> iterator = names.iterator();
+
+        //loop over the values using a while loop
+        while(iterator.hasNext()) {
+            System.out.println(iterator.next());
+        }
+    }
 }
