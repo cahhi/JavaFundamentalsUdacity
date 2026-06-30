6d9a698c9477b01842612af9707a401f1b8b716b
cahhi <chrissyrodgers20@gmail.com>
2026-06-30 09:29:32 -0500

    Created CollectionsExercise and instantiated list object

---
+++ src/main/java/genericsAndCollections/CollectionsExercise.java
@@ -0,0 +1,14 @@
+package genericsAndCollections;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class CollectionsExercise {
+
+    public static void main(String[] args){
+
+        //Instatiating list object
+        List<String> listOfItems = new LinkedList<String>();
+
+    }
+}
