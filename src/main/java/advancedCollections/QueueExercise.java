ba98dd5be167dbe08d1fff18d3b8e15e0a763c16
cahhi <chrissyrodgers20@gmail.com>
2026-07-06 14:49:49 -0500

    Created QueueExercise class and instantiated queue object

---
+++ src/main/java/advancedCollections/QueueExercise.java
@@ -0,0 +1,13 @@
+package advancedCollections;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class QueueExercise {
+
+    public static void main(String[] args){
+
+        //Instantiating a queue object
+        Queue<String> queuedCustomer = new LinkedList<>();
+    }
+}
